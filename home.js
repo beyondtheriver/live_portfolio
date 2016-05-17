ae56d2f45b794a7f41576dead0a29855616bb938
Zachary Uzupis <zuzupis@gmail.com>
2016-05-17 13:30:25 -0400

    made the landing page a smiling moustache closes #8

--- home.js
+++ home.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
    $("#header").hover(function(){
       $("#greeting")[0].play();
    })
+   if ($("#lions_roar")){
+      $("#lions_roar")[0].play();
+      setTimeout(function(){
+         $("#hidden")[0].click();
+      }, 3200);
+   }
 });
